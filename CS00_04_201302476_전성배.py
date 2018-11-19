47c4c35381ac30bded7abc9ef2719eea865ada70
JeonSeongBae <tjdqo6042@naver.com>
2018-11-19 22:16:34 +0900

    실습 2 진행중 및 보고서 양식 작성

--- CS00_04_201302476_전성배.py
+++ CS00_04_201302476_전성배.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 a = tf.constant(1, name='input_a')
 b = tf.constant(2, name='input_b')
-c = tf.add(a,b, name='sum_c')
+c = tf.add(a, b, name='sum_c')
 
 sess = tf.Session()
 writer = tf.summary.FileWriter("./board1")
